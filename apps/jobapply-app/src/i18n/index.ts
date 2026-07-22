f9376a6e53b5a59a96f947e5121490b1ddcfd1b5
Lucas Mansoldo <lucasmansoldo@Mac-mini-de-Lucas.local>
2026-07-22 14:59:26 -0300

    chore(app): remove router devtools, fix FAB position, detect browser language

--- apps/jobapply-app/src/i18n/index.ts
+++ apps/jobapply-app/src/i18n/index.ts
@@ -2,12 +2,15 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import translations from './translations.json'
 
+const browserLang = navigator.language
+const supportedLng = browserLang.startsWith('pt') ? 'pt-BR' : 'en'
+
 i18n.use(initReactI18next).init({
   resources: {
     'pt-BR': { translation: translations['pt-BR'] },
     en: { translation: translations['en'] },
   },
-  lng: 'pt-BR',
+  lng: supportedLng,
   fallbackLng: 'pt-BR',
   interpolation: {
     escapeValue: false,
